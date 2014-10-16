af9d2be7d953c7e43308960c3a0f82ad4c1e239e
Mayank Jain <mjmayank@gmail.com>
2014-10-15 21:11:32 -0500

    Initial Commit

---
+++ Groop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Groop
+//
+//  Created by Mayank Jain on 10/15/14.
+//  Copyright (c) 2014 Mayank Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Groop/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Groop
+//
+//  Created by Mayank Jain on 10/15/14.
+//  Copyright (c) 2014 Mayank Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
