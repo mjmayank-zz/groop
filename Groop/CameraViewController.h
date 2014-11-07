420abf09efe24179bbbeb5aff8e1333e764ddb13
Mayank Jain <mjmayank@gmail.com>
2014-11-07 14:00:01 -0600

    added fb sdk

---
+++ Groop/CameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  CameraViewController.h
+//  Groop
+//
+//  Created by Mayank Jain on 11/7/14.
+//  Copyright (c) 2014 Mayank Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraViewController : UIViewController
+
+@end
---
+++ Groop/CameraViewController.m
@@ -0,0 +1,37 @@
+//
+//  CameraViewController.m
+//  Groop
+//
+//  Created by Mayank Jain on 11/7/14.
+//  Copyright (c) 2014 Mayank Jain. All rights reserved.
+//
+
+#import "CameraViewController.h"
+
+@interface CameraViewController ()
+
+@end
+
+@implementation CameraViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
