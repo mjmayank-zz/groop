d0de20e692dedf88971fe32df85ef9d813f95ff3
dlquach <derekq.alt@gmail.com>
2015-01-19 08:36:31 -0600

    Opening the inbox now accepts all open invitations to lobbies

--- Groop/InboxViewController.m
+++ Groop/InboxViewController.m
@@ -17,6 +17,24 @@
 - (void) viewDidLoad {
     [super viewDidLoad];
     NSLog(@"You have opened the inbox");
+    
+    PFQuery *query = [PFQuery queryWithClassName:@"lobby"];
+    [query whereKey:@"invited" equalTo:[PFUser currentUser]];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if (!error) {
+            NSLog(@"These should be lobbies you are invited to");
+            for (PFObject *object in objects) {
+                NSLog(object[@"name"]);
+                PFRelation *invitedRelation = [object relationForKey:@"invited"];
+                PFRelation *userRelation = [object relationForKey:@"users"];
+                [invitedRelation removeObject:[PFUser currentUser]];
+                [userRelation addObject:[PFUser currentUser]];
+                [object saveInBackground];
+            }
+        }
+    }];
+    
 }
+
 
 @end
