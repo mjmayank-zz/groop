2078173198f93fe81da33cfbd32c2a4a9a94f9ae
dlquach <derekq.alt@gmail.com>
2015-01-19 08:09:56 -0600

    Created the InboxViewController to handle invite accepting and rejecting

---
+++ Groop/InboxViewController.h
@@ -0,0 +1,19 @@
+//
+//  InboxViewController.h
+//  Groop
+//
+//  Created by Derek Quach on 1/19/15.
+//  Copyright (c) 2015 Mayank Jain. All rights reserved.
+//
+
+#ifndef Groop_InboxViewController_h
+#define Groop_InboxViewController_h
+
+#import <UIKit/UIKit.h>
+
+@interface InboxViewController : UITableViewController<UITableViewDelegate>
+
+@end
+
+
+#endif
---
+++ Groop/InboxViewController.m
@@ -0,0 +1,22 @@
+//
+//  InboxViewController.m
+//  Groop
+//
+//  Created by Derek Quach on 1/19/15.
+//  Copyright (c) 2015 Mayank Jain. All rights reserved.
+//
+
+#import "InboxViewController.h"
+
+@interface InboxViewController ()
+
+@end
+
+@implementation InboxViewController
+
+- (void) viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"You have opened the inbox");
+}
+
+@end
